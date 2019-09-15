61528693f0afd66b80de3a9fd912d8a6846e0ca2
elty37 <felmeldo@gmail.com>
2019-09-16 04:53:44 +0900

    正解

--- app/webroot/hoge.php
+++ app/webroot/hoge.php
@@ -1,28 +1,25 @@
-<!DOCTYPE hyml>
+<!DOCTYPE html>
 <html>
     <head>
         <meta charset = "utf-8">
         <title>合計</title>
     </head>
     <form action = "hoge.php"method = "post">
-    りんご：<input type = "text" name = "$apple" /> <br />
-    みかん：<input type = "text" name = "$mikan" /> <br />
-    ばなな：<input type = "text" name = "$banana" /> <br />
+    りんご：<input type = "text" name = "apple" /> <br />
+    みかん：<input type = "text" name = "mikan" /> <br />
+    ばなな：<input type = "text" name = "banana" /> <br />
     <button type="submit">お題を計算</button>
     </form>
-</html>
-<? php
-//htmlの内容を受け取る
-$_post[]
-//3つの果物の値段
-$ary = "array()";
+<?php
 //ary[0]に$appleを代入
-$ary[] = $_post["apple"];
+$apple = $_POST["apple"];
 //ary[1]に$mikanを代入
-$ary[] = $_post["mikan"];
+$orange = $_POST["mikan"];
 //ary[2]に$bananaを代入
-$ary[] = $_post["banana"];
+$banana = $_POST["banana"];
 //合計
 $sum = $apple + $orange + $banana;
 //表示
-echo　"合計：", $sum, <br>
+echo "合計：" . $sum;
+?>
+</html>
