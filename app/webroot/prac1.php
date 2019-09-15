4cfaf43e67a32fd0a7b809af7541ba5f91ac5eb6
elty37 <felmeldo@gmail.com>
2019-09-16 03:21:57 +0900

    簡単な課題

---
+++ app/webroot/prac1.php
@@ -0,0 +1,28 @@
+<?php
+
+// 数値
+$a = 1;
+// 文字列
+$apple = "Apple";
+// 配列
+$ary = array();
+// $ary[0]に代入される
+$ary[] = $apple;
+// $ary[1]に代入される
+$ary[] = "banana";
+// $ary[2]に代入される
+$ary[] = "orange";
+
+//配列の中身全部表示
+print_r($ary);
+echo "<br />";
+?>
+
+
+<html>
+りんご：<input type="text" name="apple" /> <br />
+ばなな：<input type="text" name="banana" /> <br />
+みかん：<input type="text" name="orange" /> <br />
+
+<button type="submit">お題を計算</button>
+</html>
