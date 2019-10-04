8dd1c409e98d49b46bd319a7bd7afc682e078d38
elty37 <felmeldo@gmail.com>
2019-10-04 23:15:00 +0900

    とりあえずユーザ画面表示まで

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -13,16 +13,36 @@ class UsersController extends AppController {
  *
  * @var array
  */
-	public $components = array('Paginator');
-
+	public $components = array('Paginator', 'Flash');
+	public $uses = array('User', 'Role');
+	public $paginate = array(
+		'User',
+		array(
+			'page' => self::DEFALUT_PAGE,
+			'fields' => array('User.id', 'User.role_id', 'User.created', 'User.modified',),
+			'limit' => self::DEFALUT_PER_PAGE,
+			'order' => array(
+				'Post.title' => 'asc'
+			)	
+		),
+    );
 /**
  * index method
  *
  * @return void
  */
-	public function index() {
-		$this->User->recursive = 0;
-		$this->set('users', $this->Paginator->paginate());
+	public function index($id = null) {
+		if ($this->request->is('post')) {
+			if (is_null($id)) {
+				$this->add();
+			}
+			$this->edit($id);
+		} elseif ($this->request->is('delete')) {
+			$this->delete($id);
+		} elseif ($this->request->is('get')) {
+			$this->search();
+		}
+		$this->search();
 	}
 
 /**
@@ -48,14 +68,16 @@ class UsersController extends AppController {
 	public function add() {
 		if ($this->request->is('post')) {
 			$this->User->create();
-			unset($this->request->data["User"]["user_id"]);
 			if ($this->User->save($this->request->data)) {
-	//			$this->Flash->success(__('The user has been saved.'));
+				$this->Flash->success(__('The user has been saved.'));
 				$this->redirect(array('action' => 'index'));
 			} else {
 				$this->Flash->error(__('The user could not be saved. Please, try again.'));
 			}
 		}
+		// roleの一覧を取得
+		$this->set('roles', $this->Role->find('list', array('fields' => array('Role.id', 'Role.role_name'),)));
+
 	}
 
 /**
@@ -65,13 +87,13 @@ class UsersController extends AppController {
  * @param string $id
  * @return void
  */
-	public function edit($id = null) {
+	public function edit($id = null) { 
 		if (!$this->User->exists($id)) {
 			throw new NotFoundException(__('Invalid user'));
 		}
 		if ($this->request->is(array('post', 'put'))) {
 			if ($this->User->save($this->request->data)) {
-//				$this->Flash->success(__('The user has been saved.'));
+				$this->Flash->success(__('The user has been saved.'));
 				return $this->redirect(array('action' => 'index'));
 			} else {
 				$this->Flash->error(__('The user could not be saved. Please, try again.'));
@@ -79,6 +101,8 @@ class UsersController extends AppController {
 		} else {
 			$options = array('conditions' => array('User.' . $this->User->primaryKey => $id));
 			$this->request->data = $this->User->find('first', $options);
+			// roleの一覧を取得
+			$this->set('roles', $this->Role->find('list', array('fields' => array('Role.id', 'Role.role_name'),)));
 		}
 	}
 
